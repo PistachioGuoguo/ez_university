f6d5683b021b7c54c1fa4ec7cd140732f9e3e2e8
Pistachio Guoguo <sangzizhou@gmail.com>
2020-03-22 19:58:02 -0500

    Migration - Part 4 Before Biggest Change

--- courseinfo/admin.py
+++ courseinfo/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Semester, Section, Course, Instructor, Student, Registration
+from .models import Semester, Section, Course, Instructor, Student, Registration, Period, Year
 
 # Register your models here.
+admin.site.register(Year)
+admin.site.register(Period)
 admin.site.register(Semester)
 admin.site.register(Section)
 admin.site.register(Course)
